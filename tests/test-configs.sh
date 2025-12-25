9734861361706008ce53bcbbccf579a970b22aa3
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2025-12-25 15:47:58 +0000

    Move more tests from the original repository.
    The original ontology-development-kit repository has two other types of
    configuration tests:
    
    * a handful of tests that do not use a configuration file at all;
    * a test based on an Go-inspired example configuration.
    
    We move those tests to the present repository, as they are all tests of
    the seeding system and workflows.

--- tests/test-configs.sh
+++ tests/test-configs.sh
@@ -8,6 +8,10 @@ rm -rf testenv status.txt
 GITNAME=$(git config --get user.name)
 GITEMAIL=$(git config --get user.email)
 
+run_odk_seed() {
+    uv --project .. run odk seed --gitname "$GITNAME" --gitemail "$GITEMAIL" "$@"
+}
+
 # Create native ODK environment for testing
 mkdir testenv
 uv --project .. run odk install testenv
@@ -16,8 +20,18 @@ uv --project .. run odk install testenv
 # Seed and build all the test configurations
 for t in configs/*.yaml ; do
     echo -n "$t... " >> status.txt
-    uv --project .. run odk seed --gitname "$GITNAME" --gitemail "$GITEMAIL" -c -C $t
+    run_odk_seed -c -C $t
     echo DONE >> status.txt
 done
+
+# Additional tests without an explicit config file
+run_odk_seed -c -t my-ontology1 my-ont
+run_odk_seed -c -d pato -d ro -t my-ontology2 my-ont
+run_odk_seed -c -d pato -d cl -d ro -t my-ontology3 my-ont
+
+# Additional "GO mini" test
+run_odk_seed -c -D target/go-mini \
+             -C ../examples/go-mini/project.yaml \
+             -s ../examples/go-mini/go-edit.obo
 
 rm -rf target testenv status.txt
